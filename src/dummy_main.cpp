d09c1abe3c05e2edd022b95942103dad44c2d78f
Jingwei Chen <cjw990802@gmail.com>
2025-05-13 17:51:17 -0700

    delete

--- src/dummy_main.cpp
+++
@@ -1,7 +0,0 @@
-#include "calculator_constants.hpp"
-
-int main() {
-    // This is a dummy main function to ensure the project compiles.
-    // The actual implementation will be provided in the respective source files.
-    return 0;
-}
